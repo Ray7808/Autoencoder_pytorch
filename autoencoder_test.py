085ef49c62955fec10ed2da8395f9329403192eb
Ray7808 <yuwang7808@gmail.com>
2024-07-13 23:10:59 +0800

    feat: load the MNIST dataset and show the image and profile of loaded dataset

--- autoencoder_test.py
+++ autoencoder_test.py
@@ -1,5 +1,9 @@
-# import pytorch (cpu, gpu, mac chip)
+# Import relative plugins
 import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import transforms
+import matplotlib.pyplot as plt
 
 # Using cuda/mac m1/cpu device
 print("-"*30)
@@ -13,11 +17,33 @@ else:
     device = torch.device("cpu")
     print("Using CPU")
 print("-"*30)
+
+# Hyperparameters
+BATCH_SIZE = 10
+
+# function start (main)
 def main():
-    print("hello")
-    x = torch.rand(5, 3)
-    print(x)
-    pass
+    """
+    1.Loading MNIST dataset
+    2.Testing the loaded image
+    """
+    
+    # load required dataset(download should be True if you run the program in the first time)
+    train_loader = DataLoader(datasets.MNIST('./dataset', train=True,
+        download=False, transform=transforms.ToTensor()),batch_size=BATCH_SIZE, shuffle=False
+    )
+    for data, targets in train_loader:
+        break
+    plt.figure(1)
+    print(data.shape)
+    plt.imshow(data[5,0,:,:]*255)
+    
+    plt.figure(2)
+    inputs = data.view(-1, 784)
+    print(inputs.shape)
+    plt.plot(inputs[5,:]*255)
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
